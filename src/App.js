3f6d7f92b48cb29e1526bd4e47e7977a77b727a3
Lse <821798568@>
2021-12-23 09:55:59 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Editor } from "@tinymce/tinymce-react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const handleEditorChange = async (e) => {
+    console.log("Content was updated:", e.target.getContent());
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Editor
+        tinymceScriptSrc="/tinymce.min.js"
+        init={{
+          height: 500,
+          selector: "textarea", // change this value according to your HTML
+          plugins: "paste",
+          menubar: "edit",
+          toolbar: "paste",
+          paste_remove_styles_if_webkit: false,
+          paste_enable_default_filters: false,
+          paste_filter_drop: false,
+          paste_webkit_styles: "none",
+          base_url: "/tinymce",
+        }}
+        onChange={handleEditorChange}
+      />
+    </>
   );
-}
+};
 
 export default App;
